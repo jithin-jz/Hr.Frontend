f29338e7628a96b6f2e254333c19edee26014316
jithin <jithinjzx@gmail.com>
2025-11-30 17:27:30 +0530

    vercel

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { SpeedInsights } from "@vercel/speed-insights"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <>
+      <SpeedInsights />
+      <App />
+    </>
+  </StrictMode>
 )
