dd6603ef9179602d72200dc07497dd42247a492c
jithin <jithinjzx@gmail.com>
2025-11-30 17:30:26 +0530

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { SpeedInsights } from "@vercel/speed-insights"
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <>
-      <SpeedInsights />
+      <Analytics />
       <App />
     </>
   </StrictMode>
